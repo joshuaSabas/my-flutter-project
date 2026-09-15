c2224c2f8938cc1f07518bf8fde2d89b7490e990
joshuaSabas <sabasj46@gmail.com>
2026-09-15 20:33:19 +0800

    Change navigation target from HomeScreen to MainScreen

--- FertilizerCalc/FertilizerCalc/lib/splash_screen.dart
+++ FertilizerCalc/FertilizerCalc/lib/splash_screen.dart
@@ -102,7 +102,7 @@ class _SplashScreenState extends State<SplashScreen>
         context,
         PageRouteBuilder(
           transitionDuration: const Duration(milliseconds: 700),
-          pageBuilder: (_, animation, __) => const HomeScreen(),
+          pageBuilder: (_, animation, __) => const MainScreen(),
           transitionsBuilder: (_, animation, __, child) {
             return FadeTransition(
               opacity: animation,
